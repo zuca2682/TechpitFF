a96811179e03ce5f2b9452273279a5248282e184
zuca <2682zucazuca@gmail.com>
2021-03-01 17:35:23 +0900

    Humanメソッドの宣言

--- classes/Human.php
+++ classes/Human.php
@@ -2,9 +2,26 @@
 
 class Human
 {
-//プロパティ
-const MAX_HITPOINT = 100;//最大HPの定義 定数
-public $name;//人間の名前
-public $hitPoint = 100;//現在のHP
-public $attackPoint = 20;//攻撃力
+  //プロパティ
+  const MAX_HITPOINT = 100;//最大HPの定義 定数
+  public $name;//人間の名前
+  public $hitPoint = 100;//現在のHP
+  public $attackPoint = 20;//攻撃力
+
+  //メソッド
+  //攻撃
+  public function doAttack($enemy)
+  {
+    echo "『".$this->name."』の攻撃!\n";
+    echo "【".$enemy->name."】に".$this->attackPoint. "のダメージ!\n";
+  }
+  //ダメージを受ける
+  public function tookDamage($damage)
+  {
+    $this->hitPoint -= $damage;
+    //HPが０未満にならないための処理
+    if ($this->hitPoint < 0) {
+        $this->hitPoint = 0;
+    }
+  }
 }
