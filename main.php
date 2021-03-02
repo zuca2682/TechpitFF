21a32ed9ea7e2de98208d0ecb816872006d04626
zuca <2682zucazuca@gmail.com>
2021-03-02 12:30:25 +0900

    攻撃の実装

--- main.php
+++ main.php
@@ -17,3 +17,9 @@ $goblin->name = "ゴブリン";
 echo $tiida->name.":".$tiida->hitPoint."/".$tiida::MAX_HITPOINT."\n";
 echo $goblin->name.":".$goblin->hitPoint."/".$goblin::MAX_HITPOINT."\n";
 echo "\n";
+
+//攻撃
+$tiida->doAttack($goblin);
+echo "\n";
+$goblin->doAttack($tiida);
+echo "\n";
