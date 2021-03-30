74f5b408fe1ab0ce10ec1f99b37a8c446ccaa016
zuca <2682zucazuca@gmail.com>
2021-03-30 19:27:17 +0900

    勇者クラスをオーバーライド

--- classes/Brave.php
+++ classes/Brave.php
@@ -2,5 +2,22 @@
 
 class Brave extends Human
 {
+  const MAX_HITPOINT = 120;
+  public $hitPoint = self::MAX_HITPOINT;
+  public $attackPoint = 30;
 
+  public function doAttack($enemy)
+  {
+    //乱数の発生
+    if (rand(1,3) === 1) {
+      //スキルの発動
+      echo "『" .$this->name . "』のスキルが発動した!\n";
+      echo "『ぜんりょくぎり』!!\n";
+      echo $enemy->name . "に" .$this->attackPoint*1.5 . "のダメージ!\n";
+      $enemy->tookDamage($this->attackPoint*1.5);
+    }else{
+      parent::doAttack($enemy);
+    }
+    return true;
+  }
 }
