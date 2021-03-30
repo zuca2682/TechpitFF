df0040123c056c4251973d4954904c8e5664aaca
zuca <2682zucazuca@gmail.com>
2021-03-30 15:34:41 +0900

    繰り返し処理

--- main.php
+++ main.php
@@ -13,13 +13,22 @@ $goblin = new Enemy();
 $tiida->name = "ティーダ";
 $goblin->name = "ゴブリン";
 
-//現在のHPの表示
-echo $tiida->name.":".$tiida->hitPoint."/".$tiida::MAX_HITPOINT."\n";
-echo $goblin->name.":".$goblin->hitPoint."/".$goblin::MAX_HITPOINT."\n";
-echo "\n";
+//どちらかんおHPが０になるまで繰り返す
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
 
-//攻撃
-$tiida->doAttack($goblin);
-echo "\n";
-$goblin->doAttack($tiida);
-echo "\n";
+  //現在のHPの表示
+  echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+  echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n";
+  echo "\n";
+
+  //攻撃
+  $tiida->doAttack($goblin);
+  echo "\n";
+  $goblin->doAttack($tiida);
+  echo "\n";
+
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . "　：　" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . "　：　" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
