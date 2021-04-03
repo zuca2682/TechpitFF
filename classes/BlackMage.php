97410d7686b048d4e0db00dab7c6f34f66a140e9
zuca <2682zucazuca@gmail.com>
2021-04-03 14:28:31 +0900

    コンストラクタの追加

--- classes/BlackMage.php
+++ classes/BlackMage.php
@@ -8,6 +8,11 @@ class BlackMage extends Human
   private $attackPoint = 10;
   private $intelligence = 30;//魔法攻撃力
 
+  public function __construct($name)
+  {
+    parent::__construct($name, $this->hitPoint, $this->attackPoint);
+  }
+
   public function doAttack($enemy)
   {
     if (rand(1, 2) === 1) {
--- classes/WhiteMage.php
+++ classes/WhiteMage.php
@@ -7,6 +7,11 @@ class WhiteMage extends Human
   private $attackPoint = 10;
   private $intelligence = 30;//魔法攻撃力
 
+  public function __construct($name)
+  {
+    parent::__construct($name, $this->hitPoint, $this->attackPoint);
+  }
+
   public function doAttackWhiteMage($enemy, $human)
   {
     if (rand(1, 2) === 1) {
