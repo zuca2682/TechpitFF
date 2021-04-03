7a1b0b38031e3e2b06249e8388beb665666f5126
zuca <2682zucazuca@gmail.com>
2021-04-03 14:42:40 +0900

    現在のHPの表示

--- main.php
+++ main.php
@@ -28,8 +28,13 @@ while ($tiida->getHitPoint() > 0 && $goblin->getHitPoint() > 0) {
   echo "*** $turn ターン目 ***\n\n";
 
   //現在のHPの表示
-  echo $tiida->getName() . " ： " . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-  echo $goblin->getName() . " ： " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n";
+  foreach ($members as $member) {
+    echo $member->getName() . " ： " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+  }
+  echo "\n";
+  foreach ($enemies as $enemy) {
+    echo $enemy->getName() . " ： " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+  }
   echo "\n";
 
   //攻撃
