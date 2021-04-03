142d0b992967c040c61dbb74e5d12ec1adb11f61
zuca <2682zucazuca@gmail.com>
2021-04-03 15:31:15 +0900

    現在のHPの表示修正
    他記述間違えの修正。実行確認済み

--- main.php
+++ main.php
@@ -13,19 +13,16 @@ $members = array();
 $members[] = new Brave('ティーダ');
 $members[] = new WhiteMage('ユウナ');
 $members[] = new BlackMage('ルールー');
-//$tiida = new Brave("ティーダ");
 
 $enemies = array();
 $enemies[] = new Enemy('ゴブリン', 20);
 $enemies[] = new Enemy('ボム', 25);
 $enemies[] = new Enemy('モルボル', 30);
-//$goblin = new Enemy("ゴブリン");
 
 $turn = 1;
-
 $isFinishFlg = false;
 
-while(!$isFinishFlg) {
+while (!$isFinishFlg) {
   echo "*** $turn ターン目 ***\n\n";
 
   //現在のHPの表示
@@ -46,7 +43,7 @@ while(!$isFinishFlg) {
     if (get_class($member) == "WhiteMage") {
       $member->doAttackWhiteMage($enemy, $member);
     }else{
-      $tiida->doAttack($goblin);
+      $member->doAttack($enemy);
     }
     echo "\n";
   }
@@ -75,7 +72,7 @@ while(!$isFinishFlg) {
 
   //敵の全滅チェック
   $deathCnt = 0;//HPが０以下の敵の数
-  foreach ($enemie as $enemy) {
+  foreach ($enemies as $enemy) {
     if ($enemy->getHitPoint() > 0) {
         $isFinishFlg = false;
         break;
@@ -85,11 +82,17 @@ while(!$isFinishFlg) {
   if ($deathCnt === count($enemies)) {
       $isFinishFlg = true;
       echo "♪♪♪ファンファーレ♪♪♪\n\n";
-      break
+      break;
   }
   $turn++;
 
 }
 echo "★★★ 戦闘終了 ★★★\n\n";
-echo $tiida->getName() . " ： " . $tiida->getHitPoint() . "/" . $tiida::MAX_HITPOINT . "\n";
-echo $goblin->getName() . " ： " . $goblin->getHitPoint() . "/" . $goblin::MAX_HITPOINT . "\n\n";
+//現在のHPの表示
+foreach ($members as $member) {
+  echo $member->getName() . " ： " . $member->getHitPoint() . "/" . $member::MAX_HITPOINT . "\n";
+}
+echo "\n";
+foreach ($enemies as $enemy) {
+  echo $enemy->getName() . " ： " . $enemy->getHitPoint() . "/" . $enemy::MAX_HITPOINT . "\n";
+}
